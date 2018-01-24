0f49fe48e51353bcbc41597de969a3003e5f7796
Lloyd Grubham <grubham@gmail.com>
2018-01-23 23:09:17 -0700

    Initial commit from script

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+
+import Footer from './components/Footer/Footer';
+
+import poster from './img/poster.jpg';
+import logo from './img/sf-logo.png';
+
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      search: ''
+    }
+  }
+  handleChange(val){
+    this.setState({
+      search: val
+    })
+  }
+  search(e){
+    e.preventDefault();
+    window.location.assign(`https://stockfootage.com/?s=iceland+${this.state.search}&post_type=product`);
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <div className="bg-video-container">
+          <video className="bg-video" loop="1" autoPlay="1" playsInline="" muted="1" data-bgvideo="" poster={poster} data-bgvideo-fade-in="500" data-bgvideo-pause-after="120" data-bgvideo-show-pause-play="false" data-bgvideo-pause-play-x-pos="right" data-bgvideo-pause-play-y-pos="top">
+            <source src="/assets/bg-video.mp4" type="video/mp4"/>
+            <source src="/assets/bg-video.mp4" type="video/webm"/>
+            <source src="/assets/bg-video.mp4" type="video/ogg"/>
+          </video>
+        </div>
+
+        <div id="vidtop-content">
+          <header>
+            <div></div>
+            <a className="nav-link" href="https://stockfootage.com/">HOME</a>
+            <a className="nav-link" href="https://stockfootage.com/shop">SHOP</a>
+            <a className="nav-link" href="https://stockfootage.com/my-account">LOGIN</a>
+          </header>
+          <div className="interact-box">
+            <img src={logo} alt=""/>
+            <h1><span style={{color: 'rgb(188, 52, 38)'}}>Iceland</span>Footage.com</h1>
+            <form className="search" onSubmit={e=>this.search(e)}>
+              <input placeholder="Search" onChange={e=>this.handleChange(e.target.value)} value={this.state.search} type="text"/>
+            </form>
+          </div>
+          <div></div>
+        </div>
+
+        <div className="info-sec">
+          <section>
+            <h1>Iceland 4K and HD Royalty Free Stock Video Footage</h1>
+            <h4 className="sub-banner">IcelandFootage.com is a division of <a style={{textDecoration:'none', color: '#999'}} href="https://stockfootage.com/">StockFootage.com</a></h4>
+          </section>
+          <div className="hr-red"/>
+
+
+
+          <section className="contributors">
+            <h1><span className="contrib-show">Contributors Showcase</span> - Inspired to Create</h1>
+
+
+            <div className="contributor-previews">
+              <div className="contributor-preview">
+                <iframe title="yt1" width="350" height="195" src="https://www.youtube.com/embed/9HGfJhPqdBk" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt2" width="350" height="195" src="https://www.youtube.com/embed/SXZ3qWAvbVs" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt3" width="350" height="195" src="https://www.youtube.com/watch?v=-RIY8t6csn8&t=117s" frameBorder="0" allowFullScreen></iframe>
+              </div>
+            </div>
+
+            <div className="mobile-contributor-previews">
+              <div className="contributor-preview">
+                <iframe title="yt4" width="250" height="150" src="https://www.youtube.com/embed/9HGfJhPqdBk" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt5" width="250" height="150" src="https://www.youtube.com/embed/SXZ3qWAvbVs" frameBorder="0" allowFullScreen></iframe>
+              </div>
+              <div className="contributor-preview">
+                <iframe title="yt6" width="250" height="150" src="https://www.youtube.com/watch?v=-RIY8t6csn8&t=117s" frameBorder="0" allowFullScreen></iframe>
+              </div>
+            </div>
+
+            <br/>
+            <p>Explore Iceland with a collection of high quality royalty-free 4K and HD stock video footage of Iceland glaciers, waterfalls, tourists, green mountains, scenic views, and rushing waters.  Download this footage for your documentary, commercial, news, or corporate needs.</p>
+          </section>
+          <br/>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
